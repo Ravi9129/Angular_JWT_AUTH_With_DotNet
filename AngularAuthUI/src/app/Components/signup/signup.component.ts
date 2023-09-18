1d6cfb45c210e1add5eab233e63a3617104cdf4f
Ravi9129 <rrajput12340@gmail.com>
2023-09-18 20:25:19 +0530

    ravi

--- AngularAuthUI/src/app/Components/signup/signup.component.ts
+++ AngularAuthUI/src/app/Components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,6 +10,19 @@ export class SignupComponent {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
+  signUpForm!: FormGroup
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInint(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
   hideShowPass() {
     this.isText = !this.isText
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash"
